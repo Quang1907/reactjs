435bf75398b0e4b7cee19cba9c8cbd80c0cf1a2f
QuangCNTT <ltrinh1907@gmail.com>
2023-08-23 21:06:02 +0700

    module 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import styles from "./App.module.css";
+import "./App.css";
 
 function App() {
+  const age = 17;
+  const isGreen = true;
+
+  // if (age > 15) {
+  //   return <h1>is over age</h1>;
+  // } else {
+  //   return <h1>is under age</h1>;
+  // }
   return (
-    <div className={styles.App}>
-      <h2 className={styles.name}>Quang</h2>
+    <div className="App">
+      {age > 18 ? <h1>is over age</h1> : <h1>is under age</h1>}
+      <h2 style={{ color: isGreen ? "green" : "red" }}>this has color</h2>
+      {isGreen && <button>this is a button</button>}
     </div>
   );
 }
