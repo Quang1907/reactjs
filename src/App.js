58d4d497fad4c1917b9b7f546252be6e598f8a9c
QuangCNTT <ltrinh1907@gmail.com>
2023-08-24 11:17:02 +0700

    module 7

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [age, setAge] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const [showText, setShowText] = useState(true);
+  const [textColor, setTextColor] = useState("green");
 
-  const handleIncreaseAge = () => {
-    setAge(age + 1);
-    console.log(age);
+  const handleChangeValue = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleClickButton = () => {
+    setShowText(!showText);
   };
   return (
     <div className="App">
-      {age}
-      <button onClick={handleIncreaseAge}>Increase age</button>
+      <input type="text" value={inputValue} onChange={handleChangeValue} />
+      <button onClick={handleClickButton}>show/hide</button>
+      <button
+        onClick={() => {
+          setTextColor(textColor === "red" ? "green" : "red");
+        }}
+      >
+        change red color
+      </button>
+      {showText === true && <h1 style={{ color: textColor }}>{inputValue}</h1>}
     </div>
   );
 }
